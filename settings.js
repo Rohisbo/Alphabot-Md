5ccf623e90f0cebf1c8589d984c5f96879702b1a
Kikii-Kawaiii <101081345+Kikii-Kawaiii@users.noreply.github.com>
2022-03-08 06:52:55 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -14,24 +14,24 @@ global.APIKeys = {
 // setting 
 global.autoread = false // auto read pesan / message
 global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = false //status auto mengetik (auto typing)
-global.available = false //status online (online)
+global.autoketik = true //status auto mengetik (auto typing)
+global.available = true //status online (online)
 
 // Other
-global.botname = "Kikii Cans bot" //namabot kalian
-global.ownername= "Kxxzy`々" //nama kalian
+global.botname = "Kikii Botz" //namabot kalian
+global.ownername= "Kikii Cans" //nama kalian
 global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UCjjuhOD-Mt2XrKFptMVDysQ" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "Salsabila22@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.ownernomer = "6283808840711" // nomor wa kalian
 global.ownernomerr = "+6283808840711" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6283808840711","6283808840711","6283808840711"] //ganti agar fitur owner bisa di gunakan
-global.packname = 'Kikii Bot' //sticker wm ubah
+global.owner = ["62887435047326","6283808840711","6283808840711"] //ganti agar fitur owner bisa di gunakan
+global.packname = 'Kikii Botz' //sticker wm ubah
 global.author = '🍓🍓' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
